ed60c48a96995d59727de789d2279fe6db41362f
Christopher Lee <leecgh@gmail.com>
2024-12-02 21:09:42 +0000

    Refactor and remove redundant code from Day 2 solution

--- DayTwo/Program.cs
+++ DayTwo/Program.cs
@@ -4,15 +4,7 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        string baseDirectory = Path.GetDirectoryName(Environment.ProcessPath)!;
-        string inputFilePath = Path.Combine(baseDirectory, "input.txt");
-
-        string rawData;
-        using (FileStream fileStream = new(inputFilePath, FileMode.Open, FileAccess.Read))
-        {
-            StreamReader sr = new StreamReader(fileStream);
-            rawData = sr.ReadToEnd();
-        }
+        string rawData = GetRawData("input.txt");
 
         int safeReportCount = 0;
         string[] allRows = rawData.Split("\n");
@@ -26,10 +18,9 @@ internal class Program
                                         .Select(s => int.Parse(s))
                                         .ToArray();
 
-            List<List<int>> allPossibleReports = new List<List<int>>();
-
             bool isSafeReport = ReportHelper.SafeReportChecker(splitIntRow);
 
+            // Following if block not used for Part 1 solution
             if (!isSafeReport)
             {
                 List<List<int>> altReports = ReportHelper.GetAltReports(splitIntRow);
@@ -37,21 +28,30 @@ internal class Program
                 foreach (List<int> report in altReports)
                 {
                     isSafeReport = ReportHelper.SafeReportChecker(report.ToArray());
-
                     if (isSafeReport)
-                    {
-                        isSafeReport = true;
                         break;
-                    }
                 }
             }
 
             if (isSafeReport)
-            {
                 safeReportCount++;
-            }
         }
 
         Console.WriteLine(safeReportCount);
+    }
+
+    static string GetRawData(string fileName)
+    {
+        string baseDirectory = Path.GetDirectoryName(Environment.ProcessPath)!;
+        string inputFilePath = Path.Combine(baseDirectory, fileName);
+
+        string data;
+        using (FileStream fileStream = new(inputFilePath, FileMode.Open, FileAccess.Read))
+        {
+            StreamReader sr = new StreamReader(fileStream);
+            data = sr.ReadToEnd();
+        }
+
+        return data;
     }
 }
