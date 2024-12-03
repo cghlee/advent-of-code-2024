137a197c558573d95ab807ac1488635b04a1f58f
Christopher Lee <leecgh@gmail.com>
2024-12-03 19:12:13 +0000

    Set up file import for Day 3 tasks

---
+++ DayThree/Program.cs
@@ -0,0 +1,13 @@
+﻿using Utilities.IO;
+
+namespace DayThree;
+
+internal class Program
+{
+    static void Main()
+    {
+        string rawData = FileUtilities.GetRawData("input.txt");
+
+        // Add Part One solution here
+    }
+}
