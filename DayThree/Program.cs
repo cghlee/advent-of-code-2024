5f9234ab883102e9047434a456be1a4dadec99b9
Christopher Lee <leecgh@gmail.com>
2024-12-03 19:36:30 +0000

    Add Day 3, Part 1 solutions

--- DayThree/Program.cs
+++ DayThree/Program.cs
@@ -1,4 +1,5 @@
-﻿using Utilities.IO;
+﻿using System.Text.RegularExpressions;
+using Utilities.IO;
 
 namespace DayThree;
 
@@ -8,6 +9,20 @@ internal class Program
     {
         string rawData = FileUtilities.GetRawData("input.txt");
 
-        // Add Part One solution here
+        var trimmedMatches = Regex.Matches(rawData, @"mul\(\d+,\d+\)")
+                                  .Select(m => m.ToString()[4..^1]);
+
+        var totalSum = 0;
+        foreach (var match in trimmedMatches)
+        {
+            int[] splitMatches = match.Split(',')
+                                      .Select(s => int.Parse(s))
+                                      .ToArray();
+
+            int product = splitMatches[0] * splitMatches[1];
+            totalSum += product;
+        }
+
+        Console.WriteLine(totalSum);
     }
 }
