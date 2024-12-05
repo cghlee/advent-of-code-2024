04d08647ed5f4c36cec89c6d59bf870dd20d1097
Christopher Lee <leecgh@gmail.com>
2024-12-05 08:11:43 +0000

    Add initial Day 5 files

---
+++ DayFive/Program.cs
@@ -0,0 +1,13 @@
+﻿using Utilities.IO;
+
+namespace DayFive;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string rawData = FileUtilities.GetRawData("input.txt");
+
+        // Add Part 1 code here
+    }
+}
