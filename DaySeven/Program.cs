7ad40208c2057160febbf74d9278525f68887bcd
Christopher Lee <leecgh@gmail.com>
2024-12-07 08:39:37 +0000

    Add initial Day 7 files

---
+++ DaySeven/Program.cs
@@ -0,0 +1,13 @@
+﻿using Utilities.IO;
+
+namespace DaySeven;
+
+internal class Program
+{
+    static void Main()
+    {
+        string rawData = FileUtilities.GetRawData("input.txt");
+        
+        /* PART 1 SOLUTION BELOW */
+    }
+}
